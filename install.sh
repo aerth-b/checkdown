3e7f364f925f5d8077f16a8388109a7bb869751f
aerth <aerth@isupon.us>
2015-10-11 04:55:36 -0700

    Fix typo

--- install.sh
+++ install.sh
@@ -2,4 +2,4 @@
 
 # This installs Checkdown. Find out more at https://github.com/aerth/checkdown
 
-cat checkdown;echo "Using sudo to copy script to /usr/local/bin/";sudo cp checkdown /usr/local/bin/;sudo chown +x /usr/local/bin/checkdown
+cat checkdown;echo "Using sudo to copy script to /usr/local/bin/";sudo cp checkdown /usr/local/bin/;sudo chmod +x /usr/local/bin/checkdown
