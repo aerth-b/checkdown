0f8856d850a58468c44fd6300fde8c50429dfa44
aerth <aerth@isupon.us>
2015-10-11 04:36:55 -0700

    With install.sh

---
+++ install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# This installs Checkdown. Find out more at https://github.com/aerth/checkdown
+
+cat checkdown;echo "Using sudo to copy script to /usr/local/bin/";sudo cp checkdown /usr/local/bin/;sudo chown +x /usr/local/bin/checkdown
